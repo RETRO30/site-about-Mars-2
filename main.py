0d046eaf1c875de8cd275e440442454e6e1b0a77
Stanislav <59125462+RETRO30@users.noreply.github.com>
2021-02-23 02:52:27 +0500

    Тренировки в полёте

--- main.py
+++ main.py
@@ -9,5 +9,10 @@ def index(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<string:profession>')
+def training(profession):
+    return render_template('training.html', title='Заготовка', profession=profession)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
