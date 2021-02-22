6517afc9e81eb8845b34b9046d6ae6fc35d58004
Stanislav <59125462+RETRO30@users.noreply.github.com>
2021-02-23 03:31:31 +0500

    Список профессий

--- main.py
+++ main.py
@@ -14,5 +14,11 @@ def training(profession):
     return render_template('training.html', title='Заготовка', profession=profession)
 
 
+@app.route('/list_prof/<type_list>')
+def list_prof(type_list):
+    professions = list(map(lambda x: x.strip(), open('static/txt/professions.txt', encoding='utf8').readlines()))
+    return render_template('list_prof.html', title='Список профессий', type_list=type_list, professions=professions)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
