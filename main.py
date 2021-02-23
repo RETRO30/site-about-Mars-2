6f54d646b7ec8b84313a8b65523d7a9050fb994c
Stanislav <59125462+RETRO30@users.noreply.github.com>
2021-02-24 02:27:50 +0500

    Автоматический ответ

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+import json
 
 app = Flask(__name__)
 
@@ -18,6 +19,14 @@ def training(profession):
 def list_prof(type_list):
     professions = list(map(lambda x: x.strip(), open('static/txt/professions.txt', encoding='utf8').readlines()))
     return render_template('list_prof.html', title='Список профессий', type_list=type_list, professions=professions)
+
+
+@app.route('/answer')
+@app.route('/auto_answer')
+def answer():
+    with open('answer.json', 'rt', encoding='utf8') as f:
+        data = json.loads(f.read())
+    return render_template('auto_answer.html', **data)
 
 
 if __name__ == '__main__':
